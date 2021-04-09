12fee63c218359f78261a8da5e17db74878e4f4c
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-04-09 13:22:03 +0530

    first commit

---
+++ src/BladeCodiconsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeat3\BladeCodicons;
+
+use BladeUI\Icons\Factory;
+use Illuminate\Support\ServiceProvider;
+
+final class BladeCodiconsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->callAfterResolving(Factory::class, function (Factory $factory) {
+            $factory->add('file-icons', [
+                'path' => __DIR__.'/../resources/svg',
+                'prefix' => 'codicon',
+            ]);
+        });
+    }
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-codicon'),
+            ], 'blade-codicon');
+        }
+    }
+}
