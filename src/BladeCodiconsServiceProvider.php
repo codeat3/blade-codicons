dc01648ceb9c32b9711b9ae5b894dfd3fd257883
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-09 09:20:00 +0530

    implement config file

--- src/BladeCodiconsServiceProvider.php
+++ src/BladeCodiconsServiceProvider.php
@@ -6,17 +6,25 @@ namespace Codeat3\BladeCodicons;
 
 use BladeUI\Icons\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 final class BladeCodiconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('vscode-codicons', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'codicon',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-codicon', []);
+
+            $factory->add('codicon', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-codicon.php', 'blade-codicon');
     }
 
     public function boot(): void
@@ -25,6 +33,10 @@ final class BladeCodiconsServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/svg' => public_path('vendor/blade-codicon'),
             ], 'blade-codicon');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-codicon.php' => $this->app->configPath('blade-codicon.php'),
+            ], 'blade-codicon-config');
         }
     }
 }
