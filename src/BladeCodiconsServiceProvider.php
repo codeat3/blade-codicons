30f6c98965729d96206fc061d018532c98c44072
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 01:05:50 +0530

    fixes the icon set name

--- src/BladeCodiconsServiceProvider.php
+++ src/BladeCodiconsServiceProvider.php
@@ -17,7 +17,7 @@ final class BladeCodiconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-codicon', []);
 
-            $factory->add('codicon', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('vscode-codicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
 
     }
