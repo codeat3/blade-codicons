42c37b57b34203bdf139415ec5a2a3e3a8a1d9c1
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-07-19 21:25:05 +0530

    adding generation script & auto update action (#2)

--- config/generation.php
+++ config/generation.php
@@ -1,47 +1,37 @@
 <?php
 
+use Codeat3\BladeIconGeneration\IconProcessor;
+
 $svgNormalization = static function (string $tempFilepath, array $iconSet) {
-    $doc = new DOMDocument();
-    $doc->formatOutput = false;
-    $doc->load($tempFilepath);
-    /**
-     * @var DOMElement $svgElement
-     */
-    $svgElement = $doc->getElementsByTagName('svg')[0];
 
-    // Get initial viewBox value, when empty add safe default.
-    $vBoxValue = $svgElement->getAttribute('viewBox') ?: '0 0 16 16';
+    // perform generic optimizations
+    $iconProcessor = new IconProcessor($tempFilepath, $iconSet);
+    $iconProcessor
+        ->optimize(post: function (&$svgEL) {
 
-    // Remove all the attributes to control order of them on output
-    $svgElement->removeAttribute('width');
-    $svgElement->removeAttribute('height');
-    $svgElement->removeAttribute('viewBox');
-    $svgElement->removeAttribute('fill');
-    // For some reason PHP's DOMElement likes to put xmlns first even if you don't touch it.
-    $svgElement->removeAttributeNS('http://www.w3.org/2000/svg', null);
-    // Add them back in the correct order to match current results...
-    $svgElement->setAttribute('viewBox', $vBoxValue);
-    $svgElement->setAttributeNS(null, 'xmlns', 'http://www.w3.org/2000/svg');
-    $svgElement->setAttribute('fill', 'currentColor');
-    $doc->save($tempFilepath);
+            $vBox = $svgEL->getAttribute('viewBox') ?: '0 0 16 16';
+            $svgEL->setAttribute('viewBox', $vBox);
 
-    $svgLine = trim(file($tempFilepath)[1]);
-    file_put_contents($tempFilepath, $svgLine);
+        })
+        ->save();
 };
 
 return [
     [
         // Define a source directory for the sets like a node_modules/ or vendor/ directory...
-        'source' => __DIR__.'/../dist/src/icons',
+        'source' => __DIR__.'/../dist/src/icons/',
 
         // Define a destination directory for your icons. The below is a good default...
         'destination' => __DIR__.'/../resources/svg',
 
         // Enable "safe" mode which will prevent deletion of old icons...
-        'safe' => true,
+        'safe' => false,
 
         // Call an optional callback to manipulate the icon
         // with the pathname of the icon and the settings from above...
         'after' => $svgNormalization,
+
+        'is-solid' => true,
+
     ],
 ];
