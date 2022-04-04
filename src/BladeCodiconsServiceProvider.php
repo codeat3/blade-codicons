ee70cff5c34fe78c78ddb08973cf46f7142fd858
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-04-04 17:01:19 +0000

    Fix styling

--- src/BladeCodiconsServiceProvider.php
+++ src/BladeCodiconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeCodiconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-codicon', []);
 
-            $factory->add('vscode-codicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('vscode-codicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-codicon.php', 'blade-codicon');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-codicon.php', 'blade-codicon');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-codicon'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-codicon'),
             ], 'blade-codicon');
 
             $this->publishes([
-                __DIR__.'/../config/blade-codicon.php' => $this->app->configPath('blade-codicon.php'),
+                __DIR__ . '/../config/blade-codicon.php' => $this->app->configPath('blade-codicon.php'),
             ], 'blade-codicon-config');
         }
     }
